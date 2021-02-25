1112155d884cbc8aac01bc8265a240fff433cb2b
roong <roongsetfire@gmail.com>
2021-02-25 13:29:31 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CatagoryComponent } from './catagory/catagory.component';
+import { HomeComponent1 } from './home1/home1.component';
+import { NavBar } from './navbar/navbar.component';
+import { SearchBar } from './searchbar/searchbar.component';
+import { TopBanner } from './topbanner/topbanner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatagoryComponent,
+    HomeComponent1,
+    NavBar,
+    SearchBar,
+    TopBanner
   ],
   imports: [
     BrowserModule
